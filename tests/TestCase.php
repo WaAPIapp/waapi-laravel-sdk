167203fc240060e82608e4a0faaba492c0a75e56
WaAPIapp <info@waapi.app>
2023-09-06 17:13:59 +0200

    update test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace WaAPI\WaAPI\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use WaAPI\WaAPI\WaAPIServiceProvider;
 
@@ -12,9 +11,6 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'WaAPI\\WaAPI\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -23,14 +19,5 @@ class TestCase extends Orchestra
             WaAPIServiceProvider::class,
         ];
     }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_waapi-laravel-sdk_table.php.stub';
-        $migration->up();
-        */
-    }
 }
+
